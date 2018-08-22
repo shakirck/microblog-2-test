9df7bf6a00320b0d9cc69bb432c2d34fc4bb9d95
shakirck <shakirck333#gmail.com>
2018-08-22 18:39:54 +0530

    Users can login

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 from app import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-
-class User(db.Model):
+from flask_login import UserMixin
+from app import login
+class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -14,6 +15,8 @@ class User(db.Model):
     
     # def set_password(self,password):
     #     self.password_hash=generate_password_hash(password)
+    
+
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -30,3 +33,7 @@ class Post(db.Model):
     def __repr__(self):
         return '<post {}>'.format( self.body)
 
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
